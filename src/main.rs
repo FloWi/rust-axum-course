b92b461eb3a848cf23f527ae71a5d334cc6c888c
Florian Witteler <github@flwi.de>
2023-05-06 12:59:45 +0200

    02: quick_dev_done

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
 #![allow(unused)] // For beginning only.
 
 use std::net::SocketAddr;
-use axum::response::Html;
+use axum::response::{Html, IntoResponse};
 use axum::{Router, Server};
 use axum::handler::HandlerWithoutStateExt;
 use axum::routing::get;
 
 #[tokio::main]
 async fn main() {
-	let routes_hello = Router::new().route(
-        "/hello", get(|| async {
-            Html("Helo <strong>World!!!</strong>")
-        })
+    let routes_hello = Router::new().route(
+        "/hello", get(handler_hello),
     );
 
-    // region:     --- Start Server
-    let addr = SocketAddr::from(([0,0,0,0], 8080));
+    //use 127.0.0.1, because using 0.0.0.0 will cause macOS issue a warning at every recompile
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("->> LISTENING on {addr}\n");
     Server::bind(&addr).serve(routes_hello.into_make_service())
         .await
         .unwrap();
-    // endregion:  --- Start Server
-
 }
 
+async fn handler_hello() -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello", "HANDLER");
+    Html("Helo <strong>World!!!</strong>")
+}
