03431476027220837bff78f3f6eeaa38b3d4731c
Florian Witteler <github@flwi.de>
2023-05-06 12:47:14 +0200

    01: hello world done

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
 #![allow(unused)] // For beginning only.
 
+use std::net::SocketAddr;
+use axum::response::Html;
+use axum::{Router, Server};
+use axum::handler::HandlerWithoutStateExt;
+use axum::routing::get;
+
 #[tokio::main]
 async fn main() {
-	println!("Hello, world!");
+	let routes_hello = Router::new().route(
+        "/hello", get(|| async {
+            Html("Helo <strong>World!!!</strong>")
+        })
+    );
+
+    // region:     --- Start Server
+    let addr = SocketAddr::from(([0,0,0,0], 8080));
+    println!("->> LISTENING on {addr}\n");
+    Server::bind(&addr).serve(routes_hello.into_make_service())
+        .await
+        .unwrap();
+    // endregion:  --- Start Server
+
 }
 
