bad0e674e712abe7dd4cf0010e84c164a09c0da2
Florian Witteler <github@flwi.de>
2023-05-08 17:22:16 +0200

    15: error client server done
    using a uuid to keep track between client and server errors

--- src/error.rs
+++ src/error.rs
@@ -6,7 +6,7 @@ pub type Result<T> = core::result::Result<T, Error>;
 
 //Key Idea here: NEVER send internal details about the error to the client
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, strum_macros::AsRefStr)]
 pub enum Error {
 	LoginFail,
 	AuthFailNoAuthTokenCookie,
@@ -19,6 +19,45 @@ impl IntoResponse for Error {
 	fn into_response(self) -> Response {
 		println!("->> {:<12} - {self:?}", "INTO_RES");
 
-		(StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
+		// create a placeholder axum response
+		let mut response = StatusCode::INTERNAL_SERVER_ERROR.into_response();
+
+		// insert the Error into the response
+		response.extensions_mut().insert(self);
+
+		response
 	}
+}
+
+impl Error {
+	pub fn client_status_and_error(&self) -> (StatusCode, ClientError) {
+		match self {
+			Error::LoginFail => (StatusCode::FORBIDDEN, ClientError::LOGIN_FAIL),
+			// Auth
+			Error::AuthFailNoAuthTokenCookie
+			| Error::AuthFailCtxNotInRequestExtion
+			| Error::AuthFailTokenWrongFormat => {
+				(StatusCode::FORBIDDEN, ClientError::NO_AUTH)
+			}
+			//Model
+			Error::TicketDeleteFailedIdNotFound { .. } => {
+				(StatusCode::BAD_REQUEST, ClientError::INVALID_PARAMS)
+			}
+			// can be useful to have
+			// #[allow(unreachable_patterns)]
+			// _ => (
+			// 	StatusCode::INTERNAL_SERVER_ERROR,
+			// 	ClientError::SERVICE_ERROR,
+			// ),
+		}
+	}
+}
+
+#[derive(Debug, strum_macros::AsRefStr)]
+#[allow(non_camel_case_types)] //we want that in api-result (personal preference
+pub enum ClientError {
+	LOGIN_FAIL,
+	NO_AUTH,
+	INVALID_PARAMS,
+	SERVICE_ERROR,
 }
--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,13 @@ use axum::extract::{Path, Query};
 use axum::handler::HandlerWithoutStateExt;
 use axum::response::{Html, IntoResponse, Response};
 use axum::routing::{get, get_service};
-use axum::{middleware, Router, Server};
+use axum::{middleware, Json, Router, Server};
 use serde::Deserialize;
+use serde_json::json;
 use std::net::SocketAddr;
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
+use uuid::Uuid;
 
 mod ctx;
 mod error;
@@ -54,8 +56,35 @@ async fn main() -> Result<()> {
 async fn main_response_mapper(res: Response) -> Response {
 	//adding just an empty line in the logs for now to separate the different requests
 	println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
+	let uuid = Uuid::new_v4();
+
+	// Get the potential response error
+	let service_error = res.extensions().get::<Error>();
+	let client_status_error = service_error.map(|se| se.client_status_and_error());
+
+	// If client error, build the new response
+	let error_response =
+		client_status_error
+			.as_ref()
+			.map(|(status_code, client_error)| {
+				let client_error_body = json!({
+					"error": {
+						"type": client_error.as_ref(), //comes from strum
+						"req_uuid": uuid.to_string(),
+					}
+				});
+
+				println!("   ->> client_error_body {client_error_body}");
+
+				//Build the new response from the client_error_body
+				(*status_code, Json(client_error_body)).into_response() //* <-- dereference works, because statuscode is of type Copy
+			});
+
+	// TODO: Build and log the server log line
+	println!("   ->> server log line - {uuid} - Error: {service_error:?}");
+
 	println!();
-	res
+	error_response.unwrap_or(res)
 }
 
 fn routes_static() -> Router {
